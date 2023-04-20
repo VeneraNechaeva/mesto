2ed5fda592a6dbe01ddbe66a5a56d1da5e1050d2
Venera Nechaeva <venera_nechaeva@mail.ru>
2023-04-20 19:16:45 +0300

    refactor: создать классы

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,13 +1,9 @@
-//Шесть карточек «из коробки»
-import { initialCards } from './constants.js';
+import { openImg } from './utils.js';
 
-
-
-class Card {
+export class Card {
   constructor(data, templateSelector) {
     this._templateSelector = templateSelector;
-    this._name = data.name;
-    this._link = data.link;
+    this._data = data;
   }
 
   // Возвращаю разметку
@@ -50,60 +46,11 @@ class Card {
     const cardDeleteButton = this._element.querySelector('.element__delete-button');
     this._setEventListeners(cardLikeButton, cardDeleteButton);
 
-    this._element.querySelector('.element__image').src = this._link;
-    this._element.querySelector('.element__text').textContent = this._name;
+    this._element.querySelector('.element__image').src = this._data.link;
+    this._element.querySelector('.element__text').textContent = this._data.name;
+    openImg(this._element, this._data);
 
     return this._element;
   }
 }
-
-// Публикация карточек
-initialCards.forEach((item) => {
-  //Создание экземпляра карточки
-  const card = new Card(item, '#card');
-  // Создание карточки и возвращение наружу
-  const cardElement = card.generateCard();
-  // Добавление в DOM
-
-  document.querySelector('.elements').append(cardElement);
-});
-
-
-
-
-
-
-// const popupOpenImage = document.querySelector('.popup_img');
-// const popupImage = popupOpenImage.querySelector('.popup__image');
-// const popupText = popupOpenImage.querySelector('.popup__text');
-// const popupImageCloseButton = popupOpenImage.querySelector('.popup__close-icon');
-
-
-// // Метод обработки событий (открытия попапа карточки)
-// _handleOpenPopup() {
-//   popupOpenImage.classList.add('popup_opened');
-// }
-
-// // Метод обработки событий (закрытия попапа карточки)
-// _handleClosePopup() {
-//   popupOpenImage.classList.remove('popup_opened');
-// }
-
-// // Метод добавляющий все обработчики
-// _setEventListeners() {
-
-//   popupImage.addEventListener('click', () => {
-//     this._handleOpenPopup();
-//   });
-
-//   popupImageCloseButton.addEventListener('click', () => {
-//     this._handleClosePopup();
-//   });
-
-//   popupLikeButton.addEventListener('click', () => {
-//     this._handleClosePopup();
-//   });
-
-// }
-
 
